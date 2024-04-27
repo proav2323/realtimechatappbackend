b39d54d60fa5c549e72fe4d3e196443fd8e2d9f9
proav2323 <108835201+proav2323@users.noreply.github.com>
2024-04-27 14:45:33 -0500

    coding

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
+const { createServer } = require("http");
 const { Server } = require("socket.io");
-const io = new Server(server);
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome");
@@ -13,8 +16,6 @@ io.on("connection", (socket) => {
   console.log("a user connected");
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000 lol");
+httpServer.listen(3000, () => {
+  console.log("listening on port 3000");
 });
-
-module.exports = server;
