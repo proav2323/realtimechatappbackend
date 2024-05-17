6d3dc7fe938797c916311e6ba1f8acb96dc4d96a
proav2323 <108835201+proav2323@users.noreply.github.com>
2024-05-17 09:59:34 -0500

    coding

--- api/index.js
+++ api/index.js
@@ -4,14 +4,17 @@ const port = 3000;
 const admin = require("firebase-admin");
 const cors = require("cors");
 
+app.use(express.json());
 app.use(cors());
+
+var serviceAccount = require("./auth/auth.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.post("/send", async (req, res) => {
-  const { reciverId, senderId, message } = req.body();
+  const { reciverId, senderId, message } = req.body;
 
   if (!reciverId || !senderId || !message) {
     res.status(404).send("bad req");
@@ -50,10 +53,23 @@ app.post("/send", async (req, res) => {
           type: "chat",
         },
       });
+
+      res.status(200).send("notifcations send");
     }
   }
 });
 
 app.listen(port, () => {
+  admin.initializeApp({
+    apiKey: "AIzaSyAHtwLOet0wscGXSMFu87m7PTBexyKxxjA",
+    authDomain: "realtimechatapp-57160.firebaseapp.com",
+    projectId: "realtimechatapp-57160",
+    storageBucket: "realtimechatapp-57160.appspot.com",
+    messagingSenderId: "915369937393",
+    appId: "1:915369937393:web:025888165193142a14dc50",
+    measurementId: "G-MMCL00LR9L",
+    databaseURL: "",
+    credential: admin.credential.cert(serviceAccount),
+  });
   console.log(`Example app listening on port ${port}`);
 });
